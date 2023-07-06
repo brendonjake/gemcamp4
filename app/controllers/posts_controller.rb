587cd031607127027165cf8c2595261e80f567a7
brendonjake <macapaz.b1998@gmail.com>
2023-07-06 11:04:21 +0800

    init

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,64 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :validate_post_owner, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.includes(:categories, :user).all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:notice] = 'Post created successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = 'Post updated successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post update failed'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = 'Post destroyed successfully'
+    redirect_to posts_path
+  end
+
+  private
+
+  def validate_post_owner
+    unless @post.user == current_user
+      flash[:notice] = 'the post not belongs to you'
+      redirect_to posts_path
+    end
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def show; end
+
+  def edit; end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, category_ids: [])
+  end
+end
+
